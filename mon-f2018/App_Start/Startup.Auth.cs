2b708595ef507b2f0d926cc3c6373d41997d1732
ifotn <rich@infrontofthenet.com>
2018-10-01 18:40:41 -0400

    force ssl

--- mon-f2018/App_Start/Startup.Auth.cs
+++ mon-f2018/App_Start/Startup.Auth.cs
@@ -58,11 +58,11 @@ namespace mon_f2018
             //   appId: "",
             //   appSecret: "");
 
-            //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
-            //{
-            //    ClientId = "",
-            //    ClientSecret = ""
-            //});
+            app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
+            {
+                ClientId = System.Configuration.ConfigurationManager.AppSettings["GoogleClientId"],
+                ClientSecret = System.Configuration.ConfigurationManager.AppSettings["GoogleClientSecret"]
+            });
         }
     }
 }
