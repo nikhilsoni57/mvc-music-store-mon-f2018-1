bc4eb12c506b0c1339df06d46aa69d45e19ed59c
ifotn <rich@infrontofthenet.com>
2018-11-05 16:17:07 -0500

    lesson 9 part 1. cart

--- mon-f2018/Controllers/StoreController.cs
+++ mon-f2018/Controllers/StoreController.cs
@@ -43,5 +43,71 @@ namespace mon_f2018.Controllers
             ViewBag.genre = genre;
             return View(albums);
         }
+
+        // GET: Store/AddToCart/5
+        public ActionResult AddToCart(int AlbumId)
+        {
+            // create new cart item
+            GetCartId();
+            string CurrentCartId = Session["CartId"].ToString();
+
+            Cart cartItem = new Cart
+            {
+                AlbumId = AlbumId,
+                Count = 1,
+                DateCreated = DateTime.Now,
+                CartId = CurrentCartId
+            };
+
+            // save to db
+            db.Carts.Add(cartItem);
+            db.SaveChanges();
+
+            // show cart page
+            return RedirectToAction("ShoppingCart");
+        }
+
+        private void GetCartId()
+        {
+            // is there already a CartId?
+            if (Session["CartId"] == null)
+            {
+                // is user logged in?
+                if (User.Identity.Name == "")
+                {
+                    // generate unique id that is session-specific
+                    Session["CartId"] = Guid.NewGuid().ToString();
+                }
+                else
+                {
+                    Session["CartId"] = User.Identity.Name;
+                }
+            }
+        }
+
+        // GET: Store/ShoppingCart
+        public ActionResult ShoppingCart()
+        {
+            // get current cart for display
+            GetCartId();
+            string CurrentCartId = Session["CartId"].ToString();
+
+            var cartItems = db.Carts.Where(c => c.CartId == CurrentCartId);
+
+            // load view and pass it the list of items in this user's cart
+            return View(cartItems);
+        }
+
+        // GET: Store/RemoveFromCart/5
+        public ActionResult RemoveFromCart(int RecordId)
+        {
+            // remove item from this user's cart
+            Cart cartItem = db.Carts.SingleOrDefault(c => c.RecordId == RecordId);
+            db.Carts.Remove(cartItem);
+            db.SaveChanges();
+
+            // reload cart page
+            return RedirectToAction("ShoppingCart");
+        }
     }
 }
